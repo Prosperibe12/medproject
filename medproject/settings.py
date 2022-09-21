1dbf78269d9e4ba1599755328ba5d25ba6d442af
Prosperibe12 <prosperibe12@gmail.com>
2022-09-21 10:18:00 +0100

    resolved aws host

--- medproject/settings.py
+++ medproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gi9ss3zb@y2+r&t(d9jn4a=r#756ige_^0)20hlyhe#3c)hi+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['107.23.168.4','127.0.0.1:8000', '*']
+ALLOWED_HOSTS = ['54.165.115.27','127.0.0.1:8000', '*']
 
 
 # Application definition
